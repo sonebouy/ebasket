94307c110668816172bf5afec53a6bfb7a9ef461
sonebouy <sonebouy@hotmail.com>
2015-02-27 13:21:20 +0700

    Revert "begin"
    This reverts commit 8fc47f7a246a8f3d5f6fe4445348f34397a03154.

--- connect/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$con = mysql_connect("localhost", "wonderworl","3WQMt_4-dp") or die("Unable to connect to MySQL");
-	//$con = mysql_connect("localhost", "root","") or die("Unable to connect to MySQL");
-	mysql_select_db("wonderworl_ebasket") or die("Can't Select MySQL");
-	mysql_query("SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'", $con);
-?>
